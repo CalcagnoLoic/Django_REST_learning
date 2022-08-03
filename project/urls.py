4da035cc17fa5f8e7ff22148126cd3553578afee
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-08-03 12:27:39 +0200

    add testAPIcase

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from shop.views import CategoryViewset, ProductViewset
+from shop.views import CategoryViewset, ProductViewset, ArticleViewset
 from rest_framework import routers
 
 router = routers.SimpleRouter()
 
 router.register('category', CategoryViewset, basename='category')
 router.register('product', ProductViewset, basename='product')
+router.register('article', ArticleViewset, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,33 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
 
 
 class CategoryViewset(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
 
 
 class ProductViewset(ReadOnlyModelViewSet):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        queryset = Product.objects.filter(active=True)
+        category_id = self.request.GET.get('category_id')
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+
+class ArticleViewset(ReadOnlyModelViewSet):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        article_id = self.request.GET.get('article_id')
+        if article_id is not None:
+            queryset = queryset.filter(article_id=article_id)
+        return queryset
